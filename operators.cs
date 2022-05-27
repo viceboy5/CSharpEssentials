46862499a2d513f1da0e266749e2d4909752257d
viceboy5 <105528535+viceboy5@users.noreply.github.com>
2022-05-26 21:58:08 -0600

    fixed typo on line 14

---
+++ operators.cs
@@ -0,0 +1,9 @@
+using System;
+
+public class Operators
+{
+    public void Main()
+    {
+        
+
+    }
--- variables.cs
+++ variables.cs
@@ -11,7 +11,7 @@ public class Variables
 		heroName = Console.ReadLine();
 		Console.WriteLine("Choose between Rogue, Cleric, Fighter, Paladin, Ranger, Wizard, Sorcerer, Warlock, or Artificer.  Type your answer."); 
 		heroClass = Console.ReadLine();
-		Console.WriteLine("Hello " + heroName + "! I've heard you are the greatest " + heroClass + "in the land! Will you join me on an adventure to avenge my fallen clan?");
+		Console.WriteLine("Hello " + heroName + "! I've heard you are the greatest " + heroClass + " in the land! Will you join me on an adventure to avenge my fallen clan?");
 		Console.ReadLine();
 	}
 }
