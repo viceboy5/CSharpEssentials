d471293b03765000e9e4f464a6da442dab4478ff
viceboy5 <105528535+viceboy5@users.noreply.github.com>
2022-06-09 00:07:26 -0600

    Array/ForEach Lab with Superheroes

---
+++ LabAssignments/ForLoopsWithArraysLab.cs
@@ -0,0 +1,14 @@
+using System;
+					
+public class Program
+{
+
+	public static void Main(string[] args)
+	{
+	string[] superheroes = {"Iron Man", "Hulk", "Captain America", "Black Widow", "Hawkeye", "Thor"};
+	foreach(string i in superheroes)
+		{
+			Console.WriteLine(i);
+		}
+	}
+}
